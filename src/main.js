6d378545a59b0e5cfb786b08af0398c8afbaac8c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 16:50:29 -0300

    implement the router

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import './index.css';
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import router from './router'
 
-const app = createApp(App);
-
+const app = createApp(App).use(router);
 app.use(VueSweetalert2);
+
 
 app.mount('#app');
