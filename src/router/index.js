d2000b018c1b86e2bd9d74b1d86e3d21ce6ed0a0
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 19:02:43 -0300

    implement middleware

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Guard from '../services/middleware';
+
 import LoginView from '../views/LoginView.vue'
 
 const routes = [
   {
-    path: '/login',
+    path: '/',
     name: 'login',
     component: LoginView
   },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
     path: '/about',
     name: 'about',
+    beforeEnter: Guard.auth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
