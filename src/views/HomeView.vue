6e5c6c9ec617b4830661a83d50d6f5813551cf67
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 16:50:52 -0300

    default view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <LoginComponent>
+      
+    </LoginComponent>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginComponent from '@/components/LoginComponent.vue'
+
+export default {
+  name: 'LoginComponent',
+  components: {
+    LoginComponent
+  }
+}
+</script>
