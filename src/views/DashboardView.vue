208d123f9eee6ebf20d12e8bde945f8c99e6c557
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 20:01:55 -0300

    implement logout

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button class="bg-red-500" v-on:click="logout()">Sair</button>
+  </div>
+</template>
+
+<script>
+import Cookie from 'js-cookie'
+import router from '@/router'
+
+export default {
+  methods: {
+    logout() {
+      Cookie.remove('token')
+      router.push('/')
+    }
+  },
+}
+</script>
