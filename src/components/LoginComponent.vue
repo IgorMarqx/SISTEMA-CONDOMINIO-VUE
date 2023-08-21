53f261775783c8df0195560258afedf2869ddfb4
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 14:27:04 -0300

    created login component

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-red-500">
+        asdasd
+    </div>
+</template>
+   
+<script>
+export default {
+    name: 'LoginComponent',
+    props: {
+        msg: String
+    }
+}
+</script>
