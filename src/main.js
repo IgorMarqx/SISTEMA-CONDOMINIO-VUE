c67c94be1bbb8b5dcd80b78767710eb07a77ddd8
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-22 21:39:51 -0300

    import the font awesome

--- src/main.js
+++ src/main.js
@@ -4,13 +4,29 @@ import router from './router';
 
 // VUESWEETALERT PACKAGE
 import './index.css';
+// SWEETALERT2
 import VueSweetalert2 from 'vue-sweetalert2';
+
+// SWEETALERT2 CSS
 import 'sweetalert2/dist/sweetalert2.min.css';
+
+// FONT AWESOME CORE
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
 
 const app = createApp(App);
 
 app.use(VueSweetalert2);
 
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app');
