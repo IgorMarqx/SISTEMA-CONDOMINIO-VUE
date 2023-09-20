38b5b771c0643e5eae9305fa1e2ded416f7fb2b7
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-19 22:44:07 -0300

    implement the content by flowbite-vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'purpleAuda': '#B74585',
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'purpleAuda': '#B74585',
+            },
+        },
     },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
